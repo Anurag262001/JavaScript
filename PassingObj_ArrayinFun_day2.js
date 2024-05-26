66a2580829a1490dd701f1216d9f5d00d4b77e21
Anurag Sharma <91717812+Anurag262001@users.noreply.github.com>
2024-05-26 23:07:59 +0530

    Add files via upload

---
+++ PassingObj_ArrayinFun_day2.js
@@ -0,0 +1,18 @@
+const user = {
+    name:"anurag",
+    age:"23",
+    mailid:"mranuragsharma01@gmail.com"
+}
+function f1(anyname){
+    console.log(`user name is ${anyname.name} his age is ${anyname.age} and his mailid is ${anyname.mailid}`);
+}
+f1(user);
+
+
+const arr = [1,2,3,4,5];
+function f1(arrdata){
+    console.log(arrdata);
+    console.log(arrdata[2]);
+}
+f1(arr);
+
